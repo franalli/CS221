2a49bdde86a79b205ce1dd6d9e0bdf46809886f7
Filippo Ranalli <franalli@stanford.edu>
2017-10-16 12:05:25 -0700

    data visualization

---
+++ data_visualization.py
@@ -0,0 +1,79 @@
+#copyright: https://github.com/aqibsaeed/Urban-Sound-Classification/blob/master/Urban%20Sound%20Classification%20using%20NN.ipynb
+
+import glob
+import os
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import specgram
+from IPython import embed
+# %matplotlib inline
+plt.style.use('ggplot')
+
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 12
+plt.rcParams['axes.labelsize'] = 11
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titlesize'] = 14
+plt.rcParams['xtick.labelsize'] = 10
+plt.rcParams['ytick.labelsize'] = 10
+plt.rcParams['legend.fontsize'] = 11
+plt.rcParams['figure.titlesize'] = 13
+
+def load_sound_files(file_paths):
+    raw_sounds = []
+    for fp in file_paths:
+        X,sr = librosa.load(fp)
+        raw_sounds.append(X)
+    return raw_sounds
+
+def plot_waves(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 200)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        librosa.display.waveplot(np.array(f),sr=22050)
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 1: Waveplot',x=0.5, y=0.915,fontsize=18)
+    plt.show()
+    
+def plot_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 200)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        specgram(np.array(f), Fs=22050)
+        embed()
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 2: Spectrogram',x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+def plot_log_power_specgram(sound_names,raw_sounds):
+    i = 1
+    fig = plt.figure(figsize=(25,60), dpi = 200)
+    for n,f in zip(sound_names,raw_sounds):
+        plt.subplot(10,1,i)
+        D = librosa.logamplitude(np.abs(librosa.stft(f))**2, ref_power=np.max)
+        librosa.display.specshow(D,x_axis='time' ,y_axis='log')
+        plt.title(n.title())
+        i += 1
+    plt.suptitle('Figure 3: Log power spectrogram',x=0.5, y=0.915,fontsize=18)
+    plt.show()
+
+sound_file_paths = ["57320-0-0-7.wav","24074-1-0-3.wav","15564-2-0-1.wav","31323-3-0-1.wav","46669-4-0-35.wav",
+                   "89948-5-0-0.wav","40722-8-0-4.wav","103074-7-3-2.wav","106905-8-0-0.wav","108041-9-0-4.wav"]
+sound_names = ["air conditioner","car horn","children playing","dog bark","drilling","engine idling",
+               "gun shot","jackhammer","siren","street music"]
+
+folder_path = '/home/franalli/Documents/UrbanSound8K/audio/fold1/'
+sound_file_paths = [folder_path+item for item in sound_file_paths]
+
+raw_sounds = load_sound_files(sound_file_paths)
+plot_waves(sound_names,raw_sounds)
+plot_specgram(sound_names,raw_sounds)
+plot_log_power_specgram(sound_names,raw_sounds)
