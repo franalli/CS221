1d9b06f88cffed4221cd0310dc1d4b4825599d4b
Filippo <franalli@stanford.edu>
2017-10-31 19:28:36 -0700

    changes

--- kNN.py
+++ kNN.py
@@ -5,30 +5,31 @@ import collections
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import specgram
 from IPython import embed
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
 
 train = np.loadtxt('/home/franalli/Documents/UrbanSound8K/train')
 y_train = np.loadtxt('/home/franalli/Documents/UrbanSound8K/y_train')
 val = np.loadtxt('/home/franalli/Documents/UrbanSound8K/val')
 y_val = np.loadtxt('/home/franalli/Documents/UrbanSound8K/y_val')
 
-# train -= np.mean(train,axis=0)
-# val -= np.mean(val,axis=0)
 
-K = [i for i in range(1,100)]
-for k in K:
-    predictions = []
-    for i in range(len(val)):
-        distances = np.sum(np.abs(train-val[i]),axis=1)
-        neighbor_indeces = np.argsort(distances)[:k]
-        neighbor_labels = y_train[neighbor_indeces]
-        neighbor_count = collections.defaultdict(int)
-        for label in neighbor_labels:
-            neighbor_count[label] +=1
-        max_label = list(neighbor_count)[0]
-        for label in neighbor_count:
-            if neighbor_count[label] > neighbor_count[max_label]:
-                max_label = label
-        predictions.append(max_label)
 
-    acc = np.mean(predictions == y_val)
-    print 'k:{},acc:{}'.format(k,acc)
+for k in range(1,100):
+    clf = KNeighborsClassifier(n_neighbors=k, weights='uniform', algorithm='auto', 
+        leaf_size=30, p=1, metric='minkowski', metric_params=None, n_jobs=-1)
+
+    clf.fit(train,y_train)
+
+    val_predictions = clf.predict(val)
+    train_predictions = clf.predict(train)
+
+    val_acc = np.mean(val_predictions == y_val)
+    train_acc = np.mean(train_predictions == y_train)
+
+    # print(confusion_matrix(val_predictions,y_val))
+    print k
+    print 'train acc:{}'.format(train_acc)
+    print 'val acc:{}'.format(val_acc)
+    print
--- randomForest.py
+++ randomForest.py
@@ -8,7 +8,9 @@ from IPython import embed
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 
 train = np.loadtxt('/home/franalli/Documents/UrbanSound8K/train')
 y_train = np.loadtxt('/home/franalli/Documents/UrbanSound8K/y_train')
@@ -17,18 +19,23 @@ y_val = np.loadtxt('/home/franalli/Documents/UrbanSound8K/y_val')
 
 predictions = []
 
-clf = svm.SVC(C=1000.0, cache_size=8000, class_weight=None, coef0=0.0,
-    decision_function_shape='ovr', degree=2, gamma=1e-6, kernel='rbf',
-    max_iter=-1, probability=False, random_state=0, shrinking=True,
-    tol=0.001, verbose=False)
+
+
+clf = RandomForestClassifier(n_estimators=2000, criterion='gini', max_depth=None, 
+    min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, 
+    max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, 
+    min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=-1, 
+    random_state=0, verbose=0, warm_start=False, class_weight=None)
 
 clf.fit(train,y_train)
 
 val_predictions = clf.predict(val)
+feature_importance = clf.feature_importances_
 train_predictions = clf.predict(train)
 
 val_acc = np.mean(val_predictions == y_val)
 train_acc = np.mean(train_predictions == y_train)
+
 
 print(confusion_matrix(val_predictions,y_val))
 print 'train acc:{}'.format(train_acc)
